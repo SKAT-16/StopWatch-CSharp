c0d69a79652bcb58d7456bf1288d911a6c9996a2
SKAT-16 <nathangetachew536@gmail.com>
2024-12-09 16:27:40 +0300

    Implement stop watch console UI

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StopWatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      StopWatch sw = new StopWatch();
+      sw.OnStart += (message) => Console.WriteLine(message);
+      sw.OnStop += (message) => Console.WriteLine(message);
+      sw.OnReset += (message) => Console.WriteLine(message);
+
+      Console.WriteLine("Console Stopwatch");
+      Console.WriteLine("Commands: start (S), stop (T), reset (R), exit (E)");
+
+      while (true)
+      {
+        if (Console.KeyAvailable)
+        {
+          var key = Console.ReadKey(true).Key;
+          if (key == ConsoleKey.S) // Start
+          {
+            sw.Start();
+          }
+          else if (key == ConsoleKey.T) // Stop
+          {
+            sw.Stop();
+          }
+          else if (key == ConsoleKey.R) // Reset
+          {
+            sw.Reset();
+          }
+          else if (key == ConsoleKey.E) // Exit
+          {
+            sw.Stop();
+            break;
+          }
+        }
+      }
+
+      Console.WriteLine("Thank you for using the stopwatch! Good Bye");
+    }
+  }
+}
