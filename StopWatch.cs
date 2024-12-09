6e28d95a3a300fd17909f36bebfa677e4bc37439
SKAT-16 <nathangetachew536@gmail.com>
2024-12-09 15:46:20 +0300

    Implement stop watch reset method and format file

--- StopWatch.cs
+++ StopWatch.cs
@@ -1,37 +1,49 @@
 using System;
 
-namespace StopWatch {
-  class StopWatch {
+namespace StopWatch
+{
+  class StopWatch
+  {
 
     TimeSpan timeSpan;
     DateTime startTime;
     bool isRunning;
 
-    public StopWatch() {
+    public StopWatch()
+    {
       timeSpan = TimeSpan.Zero;
       isRunning = false;
     }
 
     public delegate void StopWatchEventHandler(string message);
-    public event StopWatchEventHandler? OnStart, OnStop, OnReset;  
+    public event StopWatchEventHandler? OnStart, OnStop, OnReset;
 
 
 
-    public void Start() {
-      if(!isRunning) {
+    public void Start()
+    {
+      if (!isRunning)
+      {
         startTime = DateTime.Now - timeSpan;
         isRunning = true;
         OnStart?.Invoke("Stopwatch started");
       }
     }
-    public void Stop() {
-      if(isRunning) {
+    public void Stop()
+    {
+      if (isRunning)
+      {
         isRunning = false;
         timeSpan = DateTime.Now - startTime;
         OnStop?.Invoke($"Stopwatch stopped. Total time:  {timeSpan:hh\\:mm\\:ss}");
       }
     }
-    public void Reset() {}
-    private void Tick() {}
+    public void Reset()
+    {
+      isRunning = false;
+      timeSpan = TimeSpan.Zero;
+      OnReset?.Invoke("Stopwatch reset");
+    }
+    private void Tick() { }
   }
 }
