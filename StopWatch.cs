88ccffd6705338192db481c40853df3caba50e68
SKAT-16 <nathangetachew536@gmail.com>
2024-12-09 15:23:19 +0300

    Create stop watch class, fields and methods

---
+++ StopWatch.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace StopWatch {
+  class StopWatch {
+
+    TimeSpan timeSpan;
+    bool isRunning;
+
+
+    void Start() {}
+    void Stop() {}
+    void Reset() {}
+    void Tick() {}
+  }
+}
