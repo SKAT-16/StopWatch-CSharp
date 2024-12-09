4aec6a3cc0b845205e6603627a75aa56b64d4ca5
SKAT-16 <nathangetachew536@gmail.com>
2024-12-09 16:50:45 +0300

    Imporve UI with backgorund colors

--- Program.cs
+++ Program.cs
@@ -7,22 +7,28 @@ namespace StopWatch
 
     static void HandleStart(string message)
     {
-      Console.WriteLine("Start event triggered");
+      Console.WriteLine("\nStart event triggered");
       Console.WriteLine(message);
     }
 
     static void HandleStop(string message)
     {
-      Console.WriteLine("Stop event triggered");
+      Console.WriteLine("\nStop event triggered");
       Console.WriteLine(message);
     }
     static void HandleReset(string message)
     {
-      Console.WriteLine("Reset event triggered");
+      Console.WriteLine("\nReset event triggered");
       Console.WriteLine(message);
     }
+
+
     static void Main(string[] args)
     {
+      Console.BackgroundColor = ConsoleColor.Black;
+      Console.ForegroundColor = ConsoleColor.White;
+      Console.Clear();
+
       StopWatch sw = new StopWatch();
       sw.OnStart += HandleStart;
       sw.OnStop += HandleStop;
@@ -30,6 +36,12 @@ namespace StopWatch
 
       Console.WriteLine("Console Stopwatch");
       Console.WriteLine("Commands: start (S), stop (T), reset (R), exit (E)");
+      Console.WriteLine("Press any key to exit");
+
+      var instructions = "Type 's' to start, 't' to stop, 'r' to reset, 'e' to exit";
+      var line = new string('-', instructions.Length);
+      Console.WriteLine(instructions);
+      Console.WriteLine(line);
 
       while (true)
       {
@@ -56,6 +68,9 @@ namespace StopWatch
         }
       }
 
+      Console.BackgroundColor = ConsoleColor.Black;
+      Console.ForegroundColor = ConsoleColor.Black;
+      Console.Clear();
       Console.WriteLine("Thank you for using the stopwatch! Good Bye");
     }
   }
