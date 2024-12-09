3f64966b2149dd71c0a1d903ffc6cab2bbe13120
SKAT-16 <nathangetachew536@gmail.com>
2024-12-09 15:42:23 +0300

    Implement stop watch start method

--- StopWatch.cs
+++ StopWatch.cs
@@ -4,16 +4,28 @@ namespace StopWatch {
   class StopWatch {
 
     TimeSpan timeSpan;
+    DateTime startTime;
     bool isRunning;
 
+    public StopWatch() {
+      timeSpan = TimeSpan.Zero;
+      isRunning = false;
+    }
+
     public delegate void StopWatchEventHandler(string message);
-    public event StopWatchEventHandler OnStart, OnStop, OnReset;  
+    public event StopWatchEventHandler? OnStart, OnStop, OnReset;  
 
 
 
-    void Start() {}
-    void Stop() {}
-    void Reset() {}
-    void Tick() {}
+    public void Start() {
+      if(!isRunning) {
+        startTime = DateTime.Now - timeSpan;
+        isRunning = true;
+        OnStart?.Invoke("Stopwatch started");
+      }
+    }
+    public void Stop() {}
+    public void Reset() {}
+    private void Tick() {}
   }
 }
