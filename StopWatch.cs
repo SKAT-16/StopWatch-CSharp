c62f1e655d2682c88efd6befba6c5f2f1af04f82
SKAT-16 <nathangetachew536@gmail.com>
2024-12-09 15:29:44 +0300

    Create stopwatch events and delegates

--- StopWatch.cs
+++ StopWatch.cs
@@ -6,6 +6,10 @@ namespace StopWatch {
     TimeSpan timeSpan;
     bool isRunning;
 
+    public delegate void StopWatchEventHandler(string message);
+    public event StopWatchEventHandler OnStart, OnStop, OnReset;  
+
+
 
     void Start() {}
     void Stop() {}
