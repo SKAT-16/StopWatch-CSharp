67b6179289b2e4b7e64a2c38df24d870c4e348ae
SKAT-16 <nathangetachew536@gmail.com>
2024-12-12 11:28:15 +0300

    Add files

--- obj/Debug/net9.0/StopWatch.AssemblyInfo.cs
+++ obj/Debug/net9.0/StopWatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0fdf83d83d5df82c309c4d257b5a988def3d3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c996072d7e4b60fa871dfb5e69096584df545f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
